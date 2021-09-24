0ac713c50653c7914f8081eb71a5dda2ad3225f5
filleokus <filleokus@gmail.com>
2021-09-24 18:11:56 +0200

    load-gen: Print stats about progress

--- cmd/load-gen/main.go
+++ cmd/load-gen/main.go
@@ -15,7 +15,13 @@ import (
 )
 
 type Logger struct {
-	quiet bool
+	quiet        bool
+	statsChannel chan stat
+}
+
+type stat struct {
+	time  time.Time
+	bytes int64
 }
 
 func main() {
@@ -27,7 +33,7 @@ func main() {
 	flag.Parse()
 
 	playlistURL := flag.Arg(0)
-	var logger = Logger{quiet: *quiet}
+	var logger = Logger{quiet: *quiet, statsChannel: make(chan stat)}
 
 	if playlistURL == "" {
 		fmt.Println("Usage: load-gen [options] [url]")
@@ -39,7 +45,8 @@ func main() {
 
 	var httpClient *http.Client
 	httpClient = &http.Client{
-		Timeout: time.Second * 3,
+		Timeout:   time.Second * 3,
+		Transport: &http.Transport{DisableCompression: true},
 	}
 	if *proxyURLString != "" {
 		proxyUrl, err := url.Parse(*proxyURLString)
@@ -52,13 +59,16 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Starting %d session(s)\n", *instances)
 	fmt.Printf("Starting %d session(s) with User-Agent %s \n", *instances, *userAgentString)
+	if *quiet {
+		fmt.Println("Will not print successful downloads, only statistics")
+	}
 	for i := 0; i < *instances; i++ {
 		time.Sleep(time.Millisecond * 100)
-		go listener.StartListener(playlistURL, *buffer, logger, httpClient)
 		go listener.StartListener(playlistURL, *buffer, logger, httpClient, *userAgentString)
 	}
+
+	go printStats(logger, 10)
 
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
@@ -74,10 +84,45 @@ func main() {
 
 func (l Logger) SuccessfullyDownloaded(message listener.SuccessMessage) {
 	if !l.quiet {
-		fmt.Printf("%-4d ms %s %s\n", message.Duration.Milliseconds(), message.Time.Format(time.RFC3339), message.URL)
+		fmt.Printf("%-4d ms %s %s %s\n", message.Duration.Milliseconds(), ByteCountSI(int64(message.Bytes)), message.Time.Format(time.RFC3339), message.URL)
 	}
+	l.statsChannel <- stat{time.Now(), int64(message.Bytes)}
 }
 
 func (l Logger) ErrorWhileDownloading(playbackError listener.PlaybackError) {
 	fmt.Printf("%+v\n", playbackError)
+}
+
+func printStats(l Logger, printInterval int) {
+	var total int64
+	var lastStat stat
+	firstStat := <-l.statsChannel
+	total = firstStat.bytes
+	deadline := time.Now().Add(time.Second * time.Duration(printInterval))
+	for stat := range l.statsChannel {
+		total += stat.bytes
+		lastStat = stat
+		if time.Now().After(deadline) {
+			duration := lastStat.time.Sub(firstStat.time)
+			rate := int64(float64(total) / duration.Seconds())
+			fmt.Printf("Last %.2f seconds: Transfered data: %-8s Avg: %s/s\n", duration.Seconds(), ByteCountSI(total), ByteCountSI(rate))
+			firstStat = stat
+			total = 0
+			deadline = time.Now().Add(time.Second * time.Duration(printInterval))
+		}
+	}
+}
+
+func ByteCountSI(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
 }
