c692e9b27297a8eeca82ee4126017cc6a2c73b56
filleokus <filleokus@gmail.com>
2021-09-25 09:38:21 +0200

    load-gen: Add build-script

---
+++ cmd/load-gen/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+VERSION=0.01
+
+GOOS=darwin GOARCH=amd64 go build -trimpath -o load-gen-mac-amd64-$VERSION
+GOOS=linux GOARCH=amd64 go build -trimpath -o load-gen-linux-amd64-$VERSION
+GOOS=windows GOARCH=amd64 go build -trimpath -o load-gen-windows-amd64-$VERSION.exe
